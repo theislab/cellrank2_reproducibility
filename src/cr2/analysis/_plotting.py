96d64f7248ab42349db08c0647c6a57f4ce7eebc
Philipp Weiler <weiler.philipp@gmail.com>
2023-04-25 23:23:42 +0200

    Add `plot_states` (#10)
    Add function to plot macrostates (terminal states) with outlines on a
    given embedding.

--- src/cr2/analysis/_plotting.py
+++ src/cr2/analysis/_plotting.py
@@ -1,9 +1,11 @@
-from typing import Optional
+from typing import Literal, Optional
 
 import pandas as pd
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+import scvelo as scv
 
 
 def plot_state_purity(state_purity, color="grey", fpath: Optional[str] = None, format: str = "eps"):
@@ -19,6 +21,45 @@ def plot_state_purity(state_purity, color="grey", fpath: Optional[str] = None, f
 
     fig, ax = plt.subplots(figsize=(6, 4))
     sns.barplot(data=df, x="Purity", y="State", ax=ax, **kwargs)
+
+    if fpath is not None:
+        fig.savefig(fpath, format=format, transparent=True, bbox_inches="tight")
+
+
+def plot_states(
+    adata,
+    estimator,
+    which: Literal["macrostates", "terminal_states"],
+    basis: str,
+    inplace: bool = False,
+    fpath: Optional[str] = None,
+    format: str = "eps",
+    **kwargs,
+):
+    if not inplace:
+        adata = adata.copy()
+
+    states = getattr(estimator, which).cat.categories.tolist()
+    if which == "macrostates":
+        states = estimator._macrostates
+    elif which == "terminal_states":
+        states = estimator._term_states
+    state_names = states.assignment.cat.categories.tolist()
+
+    adata.obs[which] = states.assignment.astype(str).astype("category").cat.reorder_categories(["nan"] + state_names)
+    if which == "macrostates":
+        adata.uns[f"{which}_colors"] = ["#dedede"] + states.colors
+    else:
+        adata.uns[f"{which}_colors"] = ["#dedede"] + states.colors.tolist()
+    fig, ax = plt.subplots(figsize=(6, 4))
+    scv.pl.scatter(
+        adata,
+        basis=basis,
+        c=which,
+        add_outline=state_names,
+        ax=ax,
+        **kwargs,
+    )
 
     if fpath is not None:
         fig.savefig(fpath, format=format, transparent=True, bbox_inches="tight")
