5d65c4bc891850e2a7174d15d1778673e0cd5b3e
Philipp Weiler <weiler.philipp@gmail.com>
2023-04-25 23:35:16 +0200

    Add `get_var_ranks` (#12)
    Adds functions to rank a given list of variables towards a macrostate.

--- src/cr2/analysis/__init__.py
+++ src/cr2/analysis/__init__.py
@@ -1,4 +1,5 @@
+from ._drivers import get_var_ranks
 from ._estimator import get_state_purity
 from ._plotting import plot_state_purity, plot_states
 
-__all__ = ["get_state_purity", "plot_state_purity", "plot_states"]
+__all__ = ["get_state_purity", "get_var_ranks", "plot_state_purity", "plot_states"]
