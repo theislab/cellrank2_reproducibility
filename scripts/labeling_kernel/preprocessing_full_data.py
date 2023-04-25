d6e9d3b13d912a9e8003728acbf748257d25f82d
Philipp Weiler <weiler.philipp@gmail.com>
2023-04-25 19:17:33 +0000

    Add `preprocessing_full_data.ipynb`
    Adds notebook that preprocesses the full dataset instead of excluding
    Tuft cells from the beginning.

---
+++ scripts/labeling_kernel/preprocessing_full_data.py
@@ -0,0 +1,105 @@
+# %% [markdown]
+# # Intestinal organoid differentiation - Data preprocessing
+#
+# Preprocessing of scEU-seq organoid data by
+# * Excluding control cells identified via labeling time `"dmso"`
+# * Removing genes with less than 50 counts
+# * Normalizing counts
+# * Extracting 2000 highly variable features
+# * Log1p transforming total counts stored in `adata.X`
+# * Computing PCA
+# * Constructing a nearest neighbor graph with 30 neighbors and 30 principal components
+# * Computing UMAP embedding
+
+# %% [markdown]
+# ## Library imports
+
+# %%
+import os
+import sys
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import scanpy as sc
+import scvelo as scv
+
+from cr2 import running_in_notebook
+
+sys.path.extend(["../../", "."])
+from paths import DATA_DIR, FIG_DIR  # isort: skip  # noqa: E402
+
+# %% [markdown]
+# ## General settings
+
+# %%
+sc.settings.verbosity = 3
+scv.settings.verbosity = 3
+
+# %%
+scv.settings.set_figure_params("scvelo")
+
+# %%
+SAVE_FIGURES = True
+
+if SAVE_FIGURES:
+    os.makedirs(FIG_DIR / "labeling_kernel", exist_ok=True)
+
+# %% [markdown]
+# ## Data loading
+
+# %%
+adata = sc.read(DATA_DIR / "sceu_organoid" / "processed" / "raw.h5ad")
+
+adata = adata[adata.obs["labeling_time"] != "dmso", :].copy()
+adata.obs["labeling_time"] = adata.obs["labeling_time"].astype(float) / 60
+
+adata.obs.drop(["well_id", "batch_id", "log10_gfp", "monocle_branch_id", "monocle_pseudotime"], axis=1, inplace=True)
+
+del (
+    adata.layers["labeled_unspliced"],
+    adata.layers["labeled_spliced"],
+    adata.layers["unlabeled_unspliced"],
+    adata.layers["unlabeled_spliced"],
+)
+
+adata
+
+# %%
+pd.DataFrame(adata.obs[["labeling_time", "experiment"]].groupby("experiment").apply(lambda x: x.value_counts())).rename(
+    {0: "value_counts"}, axis=1
+).droplevel(level=2).sort_index()
+
+# %%
+adata.obs["cell_type_merged"] = adata.obs["cell_type"].copy()
+adata.obs["cell_type_merged"].replace({"Enteroendocrine cells": "Enteroendocrine progenitors"}, inplace=True)
+
+# %% [markdown]
+# ## Data preprocessing
+
+# %%
+scv.pp.filter_and_normalize(
+    adata, min_counts=50, layers_normalize=["X", "labeled", "unlabeled", "total"], n_top_genes=2000
+)
+adata
+
+# %%
+sc.tl.pca(adata, svd_solver="arpack")
+sc.pp.neighbors(adata, n_neighbors=30, n_pcs=30)
+
+# %%
+sc.tl.umap(adata)
+if running_in_notebook():
+    scv.pl.scatter(adata, basis="umap", color="cell_type", legend_loc="right")
+
+if SAVE_FIGURES:
+    fig, ax = plt.subplots(figsize=(6, 4))
+    scv.pl.scatter(adata, basis="umap", color="cell_type", title="", legend_loc=False, show=False, ax=ax)
+
+    fig.savefig(
+        FIG_DIR / "labeling_kernel" / "umap_colored_by_cell_type_full_data.eps",
+        format="eps",
+        transparent=True,
+        bbox_inches="tight",
+    )
