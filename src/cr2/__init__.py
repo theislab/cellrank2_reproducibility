42e1ba9b7532a41988488b44cc75f11250be7c6c
Philipp Weiler <weiler.philipp@gmail.com>
2023-04-25 21:09:41 +0000

    Add `plot_states`
    Add function to plot macrostates (terminal states) with outlines on a
    given embedding.

--- src/cr2/__init__.py
+++ src/cr2/__init__.py
@@ -1,4 +1,4 @@
-from .analysis import get_state_purity, plot_state_purity
+from .analysis import get_state_purity, plot_state_purity, plot_states
 from .utils import running_in_notebook
 
-__all__ = ["get_state_purity", "plot_state_purity", "running_in_notebook"]
+__all__ = ["get_state_purity", "plot_state_purity", "plot_states", "running_in_notebook"]
