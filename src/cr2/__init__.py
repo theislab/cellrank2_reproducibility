6254b6facc2965f534005bb7003f3500afd2681f
Philipp Weiler <weiler.philipp@gmail.com>
2023-05-09 13:30:12 +0000

    Add `prepare_data_for_dynamo`
    Adds function to prepare AnnData instance to be used in the Dynamo
    workflow.

--- src/cr2/__init__.py
+++ src/cr2/__init__.py
@@ -1,4 +1,11 @@
-from .analysis import get_consistency, get_state_purity, get_var_ranks, plot_state_purity, plot_states
+from .analysis import (
+    get_consistency,
+    get_state_purity,
+    get_var_ranks,
+    plot_state_purity,
+    plot_states,
+    prepare_data_for_dynamo,
+)
 from .utils import get_symmetric_transition_matrix, running_in_notebook
 
 __all__ = [
@@ -8,5 +15,6 @@ __all__ = [
     "get_var_ranks",
     "plot_state_purity",
     "plot_states",
+    "prepare_data_for_dynamo",
     "running_in_notebook",
 ]
--- src/cr2/analysis/__init__.py
+++ src/cr2/analysis/__init__.py
@@ -1,6 +1,13 @@
 from ._drivers import get_var_ranks
 from ._estimator import get_state_purity
-from ._metabolic_labeling import get_consistency
+from ._metabolic_labeling import get_consistency, prepare_data_for_dynamo
 from ._plotting import plot_state_purity, plot_states
 
-__all__ = ["get_consistency", "get_state_purity", "get_var_ranks", "plot_state_purity", "plot_states"]
+__all__ = [
+    "get_consistency",
+    "get_state_purity",
+    "get_var_ranks",
+    "plot_state_purity",
+    "plot_states",
+    "prepare_data_for_dynamo",
+]
