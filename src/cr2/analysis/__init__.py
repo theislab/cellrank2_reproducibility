5a7ae4224bf0ce2c28ac89823a1828c679873a90
Philipp Weiler <weiler.philipp@gmail.com>
2024-03-17 13:41:50 +0100

    Remove `plot_states` (#55)
    * Update `dpt.ipynb`
    
    * Remove `plot_states` and use the estimator
    method `plot_macrostates`, instead.
    * Use `pathlib` to make directories instead of
    `os`.
    * Add constant `FIGURE_FORMAT`
    
    * Remove `plot_states`

--- src/cr2/analysis/__init__.py
+++ src/cr2/analysis/__init__.py
@@ -1,14 +1,13 @@
 from ._drivers import get_var_ranks
 from ._estimator import get_state_purity
 from ._metabolic_labeling import get_consistency, prepare_data_for_dynamo
-from ._plotting import plot_state_purity, plot_states, plot_tsi
+from ._plotting import plot_state_purity, plot_tsi
 
 __all__ = [
     "get_consistency",
     "get_state_purity",
     "get_var_ranks",
     "plot_state_purity",
-    "plot_states",
     "plot_tsi",
     "prepare_data_for_dynamo",
 ]
